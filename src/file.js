fc8d59ececa2dd784b66a071b99e67c686d7f74a
Sergei Nikitin <nik.pasaran@gmail.com>
2011-09-23 19:54:31 +0400

    следим за изменениями файла

--- src/file.js
+++ src/file.js
@@ -7,6 +7,11 @@ de.file = {};
 // ----------------------------------------------------------------------------------------------------------------- //
 
 de.file._cache = {};
+de.file._watched = {};
+
+// FIXME: Сейчас файл кэшируется навечно, что неправильно.
+//        Нужно или кэшировать на некоторое время (5 минут, например),
+//        или же следить за изменениями файла.
 
 de.file.get = function(filename) {
     var promise = de.file._cache[filename];
@@ -24,6 +29,17 @@ de.file.get = function(filename) {
                 promise.resolve(content);
             }
         });
+
+        if (!de.file._watched[filename]) { // FIXME: Непонятно, как это будет жить, когда файлов будет много.
+            de.file._watched[filename] = true;
+
+            // При изменении файла, удаляем его из кэша.
+            node.fs.watchFile(filename, function (curr, prev) {
+                if (curr.mtime !== prev.mtime) {
+                    delete de.file._cache[filename];
+                }
+            });
+        }
     }
 
     return promise;
