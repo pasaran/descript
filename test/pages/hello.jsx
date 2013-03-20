a1dee5a528860b71e4062d95c9f9b23b5202b362
Sergei Nikitin <nik.pasaran@gmail.com>
2013-03-20 18:38:04 +0400

    Работы по поводу #24

--- test/pages/hello.jsx
+++ test/pages/hello.jsx
@@ -1,20 +1,9 @@
-de.object({
-    username: 'nop',
-    data: {
-        foo: {
-            bar: [ 1, 2, 3 ]
-        }
+de.object(
+    {
+        username: 'nop',
     },
-    a: 42,
-    b: 24
-}, {
-    /*
-    result: {
-        foo: '.data.foo',
-        bar: '.data.foo.bar[1]',
-        sum: '.a * .b'
-    },
-    guard: '.foo'
-    */
-    template: './template.js'
-})
+    {
+        template: 'template.js',
+    }
+)
+
