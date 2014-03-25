91dabcc153c1812bb4de74a06acfc9944a275a22
edoroshenko <doroshenkoes@gmail.com>
2014-03-25 10:16:57 +0400

    added array support

--- test/pages/state.jsx
+++ test/pages/state.jsx
@@ -1,55 +1,54 @@
 de.object({
     func: de.func(function() {
             return {
-                value1: 1,
-                value2: 2,
-                value3: 3
+                idUser: '213948712394',
+                devices: {
+                    desktop: {os: 'mac'}
+                },
+                emails: [
+                    'user@yo.ru', 'user@yo.com'
+                ]
             };
         }, {
             state: function(data, context) {
                 return {
-                    function1: data.value1,
-                    function2: data.value2,
-                    function3: data.value3
+                    models: {
+                        user: {
+                            id: data.idUser,
+                            emails: data.emails
+                        },
+                        devices: data.devices
+                    }
                 }
             }
         }
     ),
-    jpath: de.func(function() {
-            return {
-                value1: {
-                    value2: {
-                        value3: 'jpath result'
-                    }
-                }
-            };
-        }, {
-            state: '.value1.value2.value3'
-        }
-    ),
     jresult: de.func(function() {
             return {
-                value1: 1,
-                value2: 2,
-                value3: 3
+                lang: 'ru',
+                settings: {
+                    view: 'tiles',
+                    hasCamera: false,
+                    hasPublished: true
+                },
+                accounts: [
+                    'user@mail.ru', 'user@gmail.com'
+                ]
             };
         }, {
             state: {
-                jresult1: ".value1",
-                jresult2: ".value2",
-                jresult3: ".value3"
+                models: {
+                    user: {
+                        language: '.lang',
+                        emails: '.accounts'
+                    },
+                    settings: '.settings'
+                }
             }
         }
     )
 }, {
     result: function(data, context) {
-        return {
-            expected: {
-                "function1":1,"function2":2,"function3":3,
-                // nothing for jpath section
-                "jresult1":1,"jresult2":2,"jresult3":3
-            },
-            result: context.state
-        };
+        return context.state;
     }
 })
